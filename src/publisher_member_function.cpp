3e773a36dfe8a7f57368b3d00fe13293ab7edd66
Mayank Deshpande <deshpandemayank5@gmail.com>
2023-11-14 23:07:40 -0500

    Added licences and performed cpplint and cppcheck

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -1,7 +1,30 @@
 /**
+Copyright © 2023 <copyright holders>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+/**
  * @file publisher_member_function.cpp
  * @author Mayank Deshpande
- * @brief ROS2 Node with a publisher and a service, demonstrating member function callbacks.
+ * @brief ROS2 Node with a publisher and a service, demonstrating member
+ * function callbacks.
  * @version 0.1
  * @date 2023-11-14
  *
@@ -15,7 +38,6 @@
 #include <string>
 #include <cpp_srvcli/srv/talkerr.hpp>
 
-#include "cpp_srvcli/srv/detail/talkerr__struct.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
@@ -23,23 +45,24 @@
  * @class ServicePublisherNode
  * @brief ROS2 Node with a publisher and a service.
  *
- * This class represents a ROS2 Node with a publisher and a service. It inherits from
- * rclcpp::Node and demonstrates the usage of member functions as callbacks for the timer
- * and service.
+ * This class represents a ROS2 Node with a publisher and a service. It inherits
+ * from rclcpp::Node and demonstrates the usage of member functions as callbacks
+ * for the timer and service.
  */
 class ServicePublisherNode : public rclcpp::Node {
  public:
   /**
    * @brief Constructor for the ServicePublisherNode class.
    *
-   * Initializes the ROS2 Node, creates a publisher, sets a publish frequency parameter,
-   * creates a service, and sets up a timer for periodic publishing.
+   * Initializes the ROS2 Node, creates a publisher, sets a publish frequency
+   * parameter, creates a service, and sets up a timer for periodic publishing.
    */
   ServicePublisherNode() : Node("service_publisher"), count_(0) {
     // Create publisher for std_msgs::msg::String messages on the "topic"
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
 
-    // Declare a parameter for the publishing frequency with a default value of 500
+    // Declare a parameter for the publishing frequency with a default value of
+    // 500
     this->declare_parameter("publish_frequency", 500);
 
     // Set default message content
@@ -48,8 +71,8 @@ class ServicePublisherNode : public rclcpp::Node {
     // Create a service for changing the message content
     service_ = this->create_service<cpp_srvcli::srv::Talkerr>(
         "change_string",
-        std::bind(&ServicePublisherNode::talkerr, this,
-                  std::placeholders::_1, std::placeholders::_2));
+        std::bind(&ServicePublisherNode::talkerr, this, std::placeholders::_1,
+                  std::placeholders::_2));
 
     // Check if the publish frequency parameter is set to the default value
     if (this->get_parameter("publish_frequency").as_int() == 500) {
@@ -59,7 +82,8 @@ class ServicePublisherNode : public rclcpp::Node {
               << this->get_parameter("publish_frequency").as_int());
     }
 
-    // Create a timer for periodic publishing with a default period of 500 milliseconds
+    // Create a timer for periodic publishing with a default period of 500
+    // milliseconds
     timer_ = this->create_wall_timer(
         std::chrono::milliseconds(500),
         std::bind(&ServicePublisherNode::timer_callback, this));
@@ -69,19 +93,17 @@ class ServicePublisherNode : public rclcpp::Node {
   /**
    * @brief Timer callback function for periodic publishing.
    *
-   * Checks the publishing frequency parameter and publishes a message if within the
-   * acceptable range. Displays warnings or errors if the frequency is too fast or too slow.
+   * Checks the publishing frequency parameter and publishes a message if within
+   * the acceptable range. Displays warnings or errors if the frequency is too
+   * fast or too slow.
    */
   void timer_callback() {
     // Check if the publish frequency is too fast or too slow
     if (this->get_parameter("publish_frequency").as_int() < 100) {
-      RCLCPP_ERROR_STREAM_THROTTLE(
-          this->get_logger(), *this->get_clock(), 100,
-          "Publish frequency too fast");
+      RCLCPP_ERROR_STREAM_THROTTLE(this->get_logger(), *this->get_clock(), 100,
+                                   "Publish frequency too fast");
     } else if (this->get_parameter("publish_frequency").as_int() > 1000) {
-      RCLCPP_FATAL_STREAM(
-          this->get_logger(),
-          "Publish frequency too fast");
+      RCLCPP_FATAL_STREAM(this->get_logger(), "Publish frequency too fast");
     }
 
     // Get the current message and publish it
@@ -96,11 +118,11 @@ class ServicePublisherNode : public rclcpp::Node {
    * Changes the message content based on the received service request.
    *
    * @param request The service request containing a new string.
-   * @param resp The service response containing the status after processing the request.
+   * @param resp The service response containing the status after processing the
+   * request.
    */
-  void talkerr(
-      const std::shared_ptr<cpp_srvcli::srv::Talkerr::Request> request,
-      std::shared_ptr<cpp_srvcli::srv::Talkerr::Response> resp) {
+  void talkerr(const std::shared_ptr<cpp_srvcli::srv::Talkerr::Request> request,
+               std::shared_ptr<cpp_srvcli::srv::Talkerr::Response> resp) {
     // Update the message content and set the response status
     this->message.data = request->new_string;
     resp->status = request->new_string;
@@ -109,10 +131,12 @@ class ServicePublisherNode : public rclcpp::Node {
   }
 
   // Member variables
-  std_msgs::msg::String message;  ///< The message to be published
+  std_msgs::msg::String message;        ///< The message to be published
   rclcpp::TimerBase::SharedPtr timer_;  ///< Timer for periodic publishing
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;  ///< Publisher for messages
-  rclcpp::Service<cpp_srvcli::srv::Talkerr>::SharedPtr service_;  ///< Service for message change
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr
+      publisher_;  ///< Publisher for messages
+  rclcpp::Service<cpp_srvcli::srv::Talkerr>::SharedPtr
+      service_;   ///< Service for message change
   size_t count_;  ///< Counter for tracking events
 };
 
--- src/subscriber_member_function.cpp
+++ src/subscriber_member_function.cpp
@@ -1,4 +1,26 @@
 /**
+Copyright © 2023 <copyright holders>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+/**
  * @file subscriber_member_function.cpp
  * @author Mayank Deshpande
  * @brief ROS2 Node with a minimal subscriber using member function callbacks.
@@ -21,16 +43,16 @@ using std::placeholders::_1;
  * @brief ROS2 Node with a minimal subscriber.
  *
  * This class represents a ROS2 Node with a minimal subscriber. It inherits from
- * rclcpp::Node and subscribes to messages on a specified topic, using a member function
- * as a callback to process received messages.
+ * rclcpp::Node and subscribes to messages on a specified topic, using a member
+ * function as a callback to process received messages.
  */
 class MinimalSubscriber : public rclcpp::Node {
  public:
   /**
    * @brief Constructor for the MinimalSubscriber class.
    *
-   * Initializes the ROS2 Node and creates a subscriber for std_msgs::msg::String
-   * messages on the specified topic.
+   * Initializes the ROS2 Node and creates a subscriber for
+   * std_msgs::msg::String messages on the specified topic.
    */
   MinimalSubscriber() : Node("minimal_subscriber") {
     // Create a subscriber for std_msgs::msg::String messages on the "topic"
@@ -42,7 +64,8 @@ class MinimalSubscriber : public rclcpp::Node {
   /**
    * @brief Callback function for processing received messages.
    *
-   * This member function is invoked whenever a new message is received on the subscribed topic.
+   * This member function is invoked whenever a new message is received on the
+   * subscribed topic.
    *
    * @param msg The received std_msgs::msg::String message.
    */
@@ -52,7 +75,8 @@ class MinimalSubscriber : public rclcpp::Node {
   }
 
   // Member variables
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;  ///< Subscriber for messages
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr
+      subscription_;  ///< Subscriber for messages
 };
 
 /**
